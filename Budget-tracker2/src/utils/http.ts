c716e28bcf9f45d0468ef09d6bcb822616480dab
umeshbogati <bogatiu17@gmail.com>
2026-03-24 13:09:03 +0545

    chore: vite env

--- Budget-tracker2/src/utils/http.ts
+++ Budget-tracker2/src/utils/http.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://localhost:3000/api",
-    headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache"
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+  },
+});
 
-http.interceptors.response.use(response => response.data,
-    error => {
-        const message = error.response.message || "Something went wrong"
+http.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    const message = error.response.message || "Something went wrong";
 
-        return Promise.reject(new Error(message))
-    }
-)
+    return Promise.reject(new Error(message));
+  },
+);
 
 export default http;
